1a06c24c564880ce288601059ea69b89abd64a36
Emre Batuhan Sungur <batuhansungur@gmail.com>
2023-12-05 18:39:06 +0200

    stack/queue operations

--- oop4/stackQueue.py
+++ oop4/stackQueue.py
@@ -1,6 +1,38 @@
 from abc import ABC, abstractmethod
 
-class Container(ABC):
+class Queue(ABC):
+    @abstractmethod
+    def enqueue(self, element):
+        pass
+
+    @abstractmethod
+    def dequeue(self):
+        pass
+
+    @abstractmethod
+    def front(self):
+        pass
+
+    @abstractmethod
+    def is_empty(self):
+        pass
+
+    @abstractmethod
+    def is_full(self):
+        pass
+
+class Stack(Collection):
+    def __init__(self, size):
+        self.size = size
+
+    @abstractmethod
+    def push(self, element):
+        pass
+
+    @abstractmethod
+    def pop(self):
+        pass
+
     @abstractmethod
     def element(self):
         pass
@@ -13,27 +45,9 @@ class Container(ABC):
     def is_full(self):
         pass
 
-class Stack(Container):
-    @abstractmethod
-    def push(self, element):
-        pass
-
-    @abstractmethod
-    def pop(self):
-        pass
-
-class Queue(Container):
-    @abstractmethod
-    def enqueue(self, element):
-        pass
-
-    @abstractmethod
-    def dequeue(self):
-        pass
-
-class ArrayUpStack(Stack, Queue):
+class ArrayUpStack(Stack):
     def __init__(self):
-        self.size = 5
+        super().__init__(size=5)
         self.stack = []
 
     def push(self, element):
@@ -54,15 +68,9 @@ class ArrayUpStack(Stack, Queue):
     def is_full(self):
         return len(self.stack) == self.size
 
-    def enqueue(self, element):
-        self.push(element)
-
-    def dequeue(self):
-        return self.pop()
-
-class ArrayDownStack(Stack, Queue):
+class ArrayDownStack(Stack):
     def __init__(self):
-        self.size = 5
+        super().__init__(size=5)
         self.stack = []
 
     def push(self, element):
@@ -83,26 +91,20 @@ class ArrayDownStack(Stack, Queue):
     def is_full(self):
         return len(self.stack) == self.size
 
-    def enqueue(self, element):
-        self.push(element)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-    def dequeue(self):
-        return self.pop()
-
-class LinkedStack(Stack, Queue):
-    class Node:
-        def __init__(self, data):
-            self.data = data
-            self.next = None
-
+class LinkedStack(Stack):
     def __init__(self):
-        self.size = 5
+        super().__init__(size=5)
         self.top = None
         self.count = 0
 
     def push(self, element):
         if not self.is_full():
-            new_node = self.Node(element)
+            new_node = Node(element)
             new_node.next = self.top
             self.top = new_node
             self.count += 1
@@ -124,26 +126,19 @@ class LinkedStack(Stack, Queue):
     def is_full(self):
         return self.count == self.size
 
-    def enqueue(self, element):
-        self.push(element)
-
-    def dequeue(self):
-        return self.pop()
-
-class ArrayQueue(Queue):
+class ArrayUpQueue(Queue):
     def __init__(self):
-        self.size = 5
+        super().__init__()
         self.queue = []
 
     def enqueue(self, element):
-        if not self.is_full():
-            self.queue.append(element)
+        self.queue.append(element)
 
     def dequeue(self):
         if not self.is_empty():
             return self.queue.pop(0)
 
-    def element(self):
+    def front(self):
         if not self.is_empty():
             return self.queue[0]
 
@@ -151,33 +146,101 @@ class ArrayQueue(Queue):
         return len(self.queue) == 0
 
     def is_full(self):
-        return len(self.queue) == self.size
+        return False
 
-array_up_stack = ArrayUpStack()
-array_down_stack = ArrayDownStack()
+class ArrayDownQueue(Queue):
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def enqueue(self, element):
+        self.queue.append(element)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+
+    def front(self):
+        if not self.is_empty():
+            return self.queue[0]
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def is_full(self):
+        return False
+
+class LinkedQueue(Queue):
+    def __init__(self):
+        super().__init__()
+        self.front_node = None
+        self.rear_node = None
+        self.count = 0
+
+    def enqueue(self, element):
+        new_node = Node(element)
+        if self.is_empty():
+            self.front_node = new_node
+            self.rear_node = new_node
+        else:
+            self.rear_node.next = new_node
+            self.rear_node = new_node
+        self.count += 1
+
+    def dequeue(self):
+        if not self.is_empty():
+            data = self.front_node.data
+            self.front_node = self.front_node.next
+            self.count -= 1
+            return data
+
+    def front(self):
+        if not self.is_empty():
+            return self.front_node.data
+
+    def is_empty(self):
+        return self.count == 0
+
+    def is_full(self):
+        return False
+
+up_stack = ArrayUpStack()
+down_stack = ArrayDownStack()
 linked_stack = LinkedStack()
-array_queue = ArrayQueue()
 
-# Push elements into the stacks and the queue
+up_queue = ArrayUpQueue()
+down_queue = ArrayDownQueue()
+linked_queue = LinkedQueue()
+
 for i in range(1, 6):
-    array_up_stack.enqueue(i * 2)
-    array_down_stack.enqueue(i * 3)
-    linked_stack.enqueue(i * 4)
-    array_queue.enqueue(i * 5)
+    up_stack.push(i * 2)
+    down_stack.push(i * 3)
+    linked_stack.push(i * 4)
 
-# Dequeue and print elements from the stacks and the queue
+    up_queue.enqueue(i * 2)
+    down_queue.enqueue(i * 3)
+    linked_queue.enqueue(i * 4)
+
 print("ArrayUpStack:")
-while not array_up_stack.is_empty():
-    print(array_up_stack.dequeue())
+while not up_stack.is_empty():
+    print(up_stack.pop())
 
 print("ArrayDownStack:")
-while not array_down_stack.is_empty():
-    print(array_down_stack.dequeue())
+while not down_stack.is_empty():
+    print(down_stack.pop())
 
 print("LinkedStack:")
 while not linked_stack.is_empty():
-    print(linked_stack.dequeue())
+    print(linked_stack.pop())
 
-print("ArrayQueue:")
-while not array_queue.is_empty():
-    print(array_queue.dequeue())
+print("ArrayUpQueue:")
+while not up_queue.is_empty():
+    print(up_queue.dequeue())
+
+print("ArrayDownQueue:")
+while not down_queue.is_empty():
+    print(down_queue.dequeue())
+
+print("LinkedQueue:")
+while not linked_queue.is_empty():
+    print(linked_queue.dequeue())
